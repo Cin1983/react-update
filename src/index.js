ae6e7757381ccd3d555ac6f9959acc2795e64006
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-05-02 17:42:20 +0200

    props

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from "react-dom";
 import Post from "./Post";
 
 const App = () => {
-    return (
-        <div>
-       <Post author="Hyacinthia" date="22 maart 2022" text="Wat een mooie website"/>
-       <Post author="Diana" date="22 maart 2022" text="Ja bedankt, ik heb echt mijn best gedaan" />
-       <Post author="Rachel" date="22 maart 2022" text="We zijn bijna klaar" />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Post
+        author="Hyacinthia"
+        date="22 maart 2022"
+        text="Wat een mooie website"
+      />
+      <Post
+        author="Diana"
+        date="22 maart 2022"
+        text="Ja bedankt, ik heb echt mijn best gedaan"
+      />
+      <Post author="Rachel" date="22 maart 2022" text="We zijn bijna klaar" />
+    </div>
+  );
+};
 
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
